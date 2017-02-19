bd778d821dabcb86ef3d7542e5abee96c05f1a06
josep.carne <josep.carne@scmspain.com>
2017-02-20 00:56:28 +0100

    Third commit

--- src/main/java/com/scmspain/codetest/service/LoginServiceImpl.java
+++ src/main/java/com/scmspain/codetest/service/LoginServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.UUID;
  * Created by josep.carne on 12/02/2017.
  */
 public class LoginServiceImpl {
+    private static final String COOKIE_HEADER = "Cookie";
 
     public boolean isValidUser(String username, String password) {
         final AccountDao accountDao = new AccountDao();
@@ -26,67 +27,18 @@ public class LoginServiceImpl {
 
     public void processLogoutGet(HttpExchange httpExchange) throws IOException {
 
-        //final Headers headers = httpExchange.getRequestHeaders();
-        //final String cookieValue = headers.getFirst(COOKIE_HEADER);
-
-        /*if (cookieValue != null) {
-            final UUID uuid = UUID.fromString(cookieValue);
-            Sessions.getInstance().removeSession(uuid);
-        }*/
+        /* TODO */
 
         httpExchange.sendResponseHeaders(200, 0);
     }
 
     public void processLoginGet(HttpExchange httpExchange) throws IOException {
 
-        /*final String requestedURI = httpExchange.getRequestURI().toString();
-        final SessionInfo sessionInfo = SessionContext.getSession();
-        final LoginFormImpl loginForm = new LoginFormImpl();
-
-        String html = "";
-        if (Sessions.getInstance().isValidSession()) {
-            html = loginForm.doNoRequiredLogin();
-            Sessions.getInstance().refreshSession(sessionInfo.getUUID(), sessionInfo.getUsername());
-        } else {
-            html = loginForm.doRequiredLogin(requestedURI);
-        }
-
-        httpExchange.sendResponseHeaders(200, html.length());
-        try (final OutputStream os = httpExchange.getResponseBody()) {
-            os.write(html.getBytes());
-        }*/
+        /* TODO */
     }
 
     public void processLoginPost(HttpExchange httpExchange) throws IOException {
 
-        /*final SessionInfo sessionInfo = SessionContext.getSession();
-
-        if (!Sessions.getInstance().isValidSession()) {
-            String body = this.getBody(httpExchange);
-            String [] formData = body.split("&");
-            if (formData.length == 2) {
-                String username = formData[0].split("=")[1];
-                String password = formData[1].split("=")[1];
-
-                LoginServiceImpl loginService = new LoginServiceImpl();
-                if (loginService.isValidUser(username, password)) {
-                    UUID uuid = UUID.randomUUID();
-                    this.setCookieHeader(httpExchange, uuid.toString());
-                    Sessions.getInstance().refreshSession(uuid, username);
-                    this.doRedirect(httpExchange);
-                } else {
-
-                    final UnAuthenticated view = new UnAuthenticated();
-                    String html = view.doAuthenticated();
-                    httpExchange.sendResponseHeaders(401, html.length());
-                    try (final OutputStream os = httpExchange.getResponseBody()) {
-                        os.write(html.getBytes());
-                    }
-                }
-            }
-        } else {
-            Sessions.getInstance().refreshSession(sessionInfo.getUUID(), sessionInfo.getUsername());
-            doRedirect(httpExchange);
-        }*/
+        /* TODO */
     }
 }
